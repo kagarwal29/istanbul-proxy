211a5c5ea16aa8af09bd762447437198b59d6519
kagarwal29 <kagarwal29@gmail.com>
2017-06-12 12:18:03 +0530

    Add files via upload

---
+++ instrumentedCode/sample.js
@@ -0,0 +1,9 @@
+
+var __cov_FqQ5DSyyt8re7fvDZa047w = (Function('return this'))();
+if (!__cov_FqQ5DSyyt8re7fvDZa047w.__coverage__) { __cov_FqQ5DSyyt8re7fvDZa047w.__coverage__ = {}; }
+__cov_FqQ5DSyyt8re7fvDZa047w = __cov_FqQ5DSyyt8re7fvDZa047w.__coverage__;
+if (!(__cov_FqQ5DSyyt8re7fvDZa047w['/Users/kapaga/workspace/CodeCoverage/workspace/sourceCode/sample.js'])) {
+   __cov_FqQ5DSyyt8re7fvDZa047w['/Users/kapaga/workspace/CodeCoverage/workspace/sourceCode/sample.js'] = {"path":"/Users/kapaga/workspace/CodeCoverage/workspace/sourceCode/sample.js","s":{"1":0,"2":0,"3":0},"b":{"1":[0,0]},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":6}},"2":{"start":{"line":2,"column":0},"end":{"line":3,"column":6}},"3":{"start":{"line":3,"column":1},"end":{"line":3,"column":6}}},"branchMap":{"1":{"line":2,"type":"if","locations":[{"start":{"line":2,"column":0},"end":{"line":2,"column":0}},{"start":{"line":2,"column":0},"end":{"line":2,"column":0}}]}}};
+}
+__cov_FqQ5DSyyt8re7fvDZa047w = __cov_FqQ5DSyyt8re7fvDZa047w['/Users/kapaga/workspace/CodeCoverage/workspace/sourceCode/sample.js'];
+__cov_FqQ5DSyyt8re7fvDZa047w.s['1']++;var x;__cov_FqQ5DSyyt8re7fvDZa047w.s['2']++;if(x){__cov_FqQ5DSyyt8re7fvDZa047w.b['1'][0]++;__cov_FqQ5DSyyt8re7fvDZa047w.s['3']++;x=46;}else{__cov_FqQ5DSyyt8re7fvDZa047w.b['1'][1]++;}
